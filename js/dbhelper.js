69c9af359e083fedbf3e5d0f717ba14fe6319a94
Bartol Bilankov <bartol.bilankov@gmail.com>
2018-06-11 17:26:14 -0400

    test 6 port

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -8,7 +8,7 @@ class DBHelper {
    * Change this to restaurants.json file location on your server.
    */
   static get DATABASE_URL() {
-    const port = 5500 // Change this to your server port
+     // Change this to your server port
     return `https://msmatki.github.io/Restaurant-app/data/restaurants.json`;
   }
 
